96e2211aa97336baf09444f8f47086cbdd36c7ad
ildar <Ildar626gtr@gamail.com>
2024-11-22 23:17:20 +0500

    review -> small change

--- backend/app/bot/handlers/user_handlers.py
+++ backend/app/bot/handlers/user_handlers.py
@@ -3,20 +3,22 @@ from telegram.ext import ContextTypes, CommandHandler, MessageHandler, filters, 
 
 from app.bot.services.user_service import get_or_create, get_user_by_id
 
-ASK_PERMISSION, CONTINUE_CONVERSATION = range(2)
+ASK_PERMISSION = 1
+
 
 async def start(update: Update, context: CallbackContext) -> int:
     await update.message.reply_text("Здравствуйте!")
     permission = await get_permission(update, context)
     return permission
 
+
 async def create_user(update: ContextTypes.DEFAULT_TYPE, context: CallbackContext):
     user, _ = await get_or_create(id=update.effective_user.id,
                                   first_name=update.effective_user.first_name,
-                                  username=update.effective_user.username,
-                                  chat_id=update.effective_chat.id)
+                                  username=update.effective_user.username)
 
     await update.message.reply_text(text=f'Привет {user.first_name}, я **DEV** бот, если я сломаюсь - не страшно')
+
 
 async def get_permission(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_keyboard = [["Разрешить"]]
@@ -29,19 +31,21 @@ async def get_permission(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
         await create_user(update, context)
 
-        return CONTINUE_CONVERSATION
-     
+        return ConversationHandler.END
+
     await update.message.reply_text(
         "Разрешите использовать данные вашего аккаунта для работы бота",
         reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, resize_keyboard=True, 
+            reply_keyboard, one_time_keyboard=True, resize_keyboard=True,
         ),
     )
-    
-    return ASK_PERMISSION  
-    
-def cancel(update: Update, context: CallbackContext) -> int:
+
+    return ASK_PERMISSION
+
+
+async def cancel(update: Update, context: CallbackContext) -> int:
     return ConversationHandler.END
+
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
@@ -58,25 +62,22 @@ async def me(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = await get_user_by_id(update.message.from_user.id)
     user_data_db = '\n'.join([f'external_id: {user.external_id}',
                               f'first_name: {user.first_name}',
-                              f'username: {user.username}',
-                              f'chat_id: {user.chat_id}'])
+                              f'username: {user.username}'])
     user_data_tg = '\n'.join([f'external_id: {update.effective_user.id}',
                               f'first_name: {update.effective_user.first_name}',
-                              f'username: {update.effective_user.name}',
-                              f'chat_id: {update.effective_chat.id}'])
+                              f'username: {update.effective_user.name}'])
     msg = 'Таким я тебя помню:\n' + user_data_db + '\n\n' + 'Такой ты сейчас\n' + user_data_tg
     await update.message.reply_text(text=msg)
 
 
 # start_handler = CommandHandler('start', start)
 permission_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
-        states={
-            ASK_PERMISSION: [MessageHandler(filters.TEXT & (~filters.COMMAND), get_permission)],
-            CONTINUE_CONVERSATION: [MessageHandler(filters.TEXT & (~filters.COMMAND), echo)],
-        },
-        fallbacks=[CommandHandler('cancel', cancel)]
-    )
+    entry_points=[CommandHandler("start", start)],
+    states={
+        ASK_PERMISSION: [MessageHandler(filters.TEXT & (~filters.COMMAND), get_permission)]
+    },
+    fallbacks=[CommandHandler('cancel', cancel)]
+)
 personal_info_handler = CommandHandler('me', me)
 echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
 help_handler = CommandHandler('help', help_command)
--- backend/app/bot/services/user_service.py
+++ backend/app/bot/services/user_service.py
@@ -1,20 +1,20 @@
-from app.internal.models.tg_user import TGUser
+from app.internal.models.tg_user_model import TGUser
 
 
 async def get_user_by_id(id):
     return await TGUser.objects.filter(external_id=id).afirst()
 
 
-async def create_user(id, username, chat_id, first_name=''):
+async def create_user(id, username, age=0, first_name=''):
     return await TGUser.objects.acreate(exteranl_id=id,
                                         first_name=first_name,
                                         username=username,
-                                        chat_id=chat_id)
+                                        age=age)
 
 
-async def get_or_create(id, first_name, username, chat_id):
+async def get_or_create(id, first_name, username, age=0):
     return await TGUser.objects.aget_or_create(external_id=id,
                                                defaults={
                                                    'first_name': first_name,
                                                    'username': username,
-                                                   'chat_id': chat_id})
+                                                   'age': age})
