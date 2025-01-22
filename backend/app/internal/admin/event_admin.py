ef6d6e12c23d05e6e659e8d59d4a8c829c80b728
ildar <Ildar626gtr@gamail.com>
2025-01-22 14:45:01 +0500

    change event django-admin page

--- backend/app/internal/admin/event_admin.py
+++ backend/app/internal/admin/event_admin.py
@@ -2,7 +2,14 @@ from app.internal.models.event_model import Event
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
+from app.internal.models.registration_event_model import RegistrationEvent
+
+
+class EventUsersInline(admin.TabularInline):
+    model = RegistrationEvent
+    extra = 1
+
 
 @admin.register(Event)
 class EventAdmin(ModelAdmin):
-    pass
+    inlines = (EventUsersInline,)
